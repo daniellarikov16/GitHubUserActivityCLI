d5bf0e4dfc7abe88bb1216700de12a90b8152a9a
daniellarikov16 <hopperlarikov@gmail.com>
2025-01-27 17:40:05 +0300

    Final version

--- CLI.py
+++ CLI.py
@@ -3,18 +3,75 @@ import requests
 def get_data(url):
     json_data = requests.get(url).json()
     return json_data
-username = str(input('Введите имя пользователя для посика информации: '))
-url = f'https://api.github.com/users/{username}/events'
-data = get_data(url)
-dic = []
-c = 0
+
+while True:
+    username = str(input('Введите имя пользователя для поиска информации: '))
+    url = f'https://api.github.com/users/{username}/events'
+    data = get_data(url)
+    if not(isinstance(data, list)):
+         print("Не удалось получить данные от пользователя")
+    else:
+        break
+dic ={'PushEvent':[],
+      'CreateEvent':[],
+      'DeleteEvent':[],
+      'ForkEvent':[],
+      'ReleaseEvent':[],
+      'WatchEvent':[]}
 for elem in data:
     event_type = elem.get('type')
+    if not event_type:
+        continue
     repo_info = elem.get('repo')
+    if not repo_info:
+        continue
     repo_name = repo_info.get('name')
-    dic.append(event_type)
-    dic.append(repo_name)
-    dic.append('   ')
-
-print(dic)
-print(c)
+    if not repo_name:
+        continue
+    if event_type in dic:
+        type_array = dic.get(event_type)
+        if event_type != 'PushEvent':
+            if repo_name not in type_array:
+                type_array.append(repo_name)
+                dic[event_type] = type_array
+        else:
+            type_array.append(repo_name)
+            dic[event_type] = type_array
+for key in dic:
+    if dic.get(key):
+        if key == 'PushEvent':
+            push_array = dic.get('PushEvent')
+            while push_array:
+                elem0 = push_array[0]
+                c = 0
+                for elem in push_array:
+                    if elem0 != elem:
+                        break
+                    c+= 1
+                print(f'Пользователь отправил {c} commits в репозиторий {elem0}.')
+                push_array = push_array[c:]
+        if key  == 'CreateEvent':
+            create_array = dic.get('CreateEvent')
+            while create_array:
+                print(f'Пользователь создал {create_array[0]}.')
+                create_array.pop(0)
+        if key == 'DeleteEvent':
+            delete_array = dic.get('DeleteEvent')
+            while delete_array:
+                print(f'Пользователь удалил {delete_array[0]}.')
+                delete_array.pop(0)
+        if key == 'ForkEvent':
+            fork_array = dic.get('ForkEvent')
+            while fork_array:
+                print(f'Пользователь создал копию (fork) {fork_array[0]}.')
+                fork_array.pop(0)
+        if key == 'ReleaseEvent':
+            release_array = dic.get('ReleaseEvent')
+            while release_array:
+                print(f'Пользователь создал релиз в {release_array[0]}.')
+                release_array.pop(0)
+        if key == 'WatchEvent':
+            watch_array = dic.get('WatchEvent')
+            while watch_array:
+                print(f'Пользователь начал отслеживать {watch_array[0]}.')
+                watch_array.pop(0)
